464390e685ff1c62a7d0f39995fa22eba1689a1c
John Paz <johnapaz@gmail.com>
2021-07-23 17:18:29 -0400

    Initial commit

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Internazionale Orlando Soccer Club',
+  shortName: 'LAM',
+  description: 'The Beautiful Game In The Beautiful City',
+  url: 'https://interorlando.soccer', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Internazionale Orlando SC',
+  twitter: ''
+}
